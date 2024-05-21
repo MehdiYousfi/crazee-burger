88e618a54acdf9eef09ef8f86c97036e40a1c2dc
Mehdi Yousfi <Mehdi_Ysf@outlook.fr>
2024-05-21 13:48:48 +0200

    F03: LOGIN > Add styling background

--- src/components/pages/login/LoginPage.jsx
+++ src/components/pages/login/LoginPage.jsx
@@ -18,13 +18,28 @@ export default function LoginPage() {
 }
 
 const LoginPagStyled = styled.div`
-  background: red;
   width: 100vw;
   height: 100vh;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
+
+  ::before {
+    content: "";
+    background: url("/images/burger-background.jpg") rgba(0, 0, 0, 0.7);
+    background-size: cover;
+    background-position: center;
+    background-blend-mode: darken;
+
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    z-index: -1;
+  }
+
 `;
 
 
