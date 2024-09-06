32214439ba7c49f116a7dac6cb5b748d204bc434
Mehdi Yousfi <Mehdi_Ysf@outlook.fr>
2024-09-06 17:26:31 +0200

    ✨ F04: ORDER > Navbar > Create Profile

--- src/components/pages/order/NavbarRightSide.jsx
+++ src/components/pages/order/NavbarRightSide.jsx
@@ -1,18 +1,34 @@
 import { Link } from "react-router-dom";
 import styled from "styled-components";
+import Profile from "./Profile";
 
 export default function NavbarRightSide({username}) {
   return (
     <NavbarRightSideStyled className="right-side">
-    <h1>Bonjour {username}</h1>
-    <Link to="/">
-      <button>Déconnexion</button>
-    </Link>
+      {/* <div className="admin-button">Admin Button</div> */}
+    {/* <div className="profile">
+      <p>Bonjour {username}</p>
+      <Link to="/">
+        <button>Déconnexion</button>
+      </Link>
+    </div>
+    <div className="picture"></div> */}
+    <Profile/>
   </NavbarRightSideStyled>
   )
 }
 
 const NavbarRightSideStyled = styled.div`
-    background: purple;
+    display: flex;
+    align-items: center;
+    padding-right: 50px;
+
+    /* .admin-button{
+      background: lightblue;
+    } */
+
+    .profile {
+      background: yellow;
+    }
   
 `;
