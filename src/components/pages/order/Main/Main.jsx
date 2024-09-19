9f4a5677f82a5c7b9528e0b0a9475fbe1b3590ba
Mehdi Yousfi <Mehdi_Ysf@outlook.fr>
2024-09-19 18:00:10 +0200

    💡 F05: ORDER > MAIN > Prepare code for <Basket/>

--- src/components/pages/order/Main/Main.jsx
+++ src/components/pages/order/Main/Main.jsx
@@ -4,7 +4,7 @@ import Menu from "./Menu";
 
 export default function Main() {
   return <MainStyled className="main">
-    <div className="basket">Basket</div>
+    {/* <div className="basket">Basket</div> */}
     <Menu />
    
   </MainStyled>
@@ -20,12 +20,13 @@ const MainStyled = styled.div`
   box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
 
   display: grid;
-  grid-template-columns: 25% 75%;
+ 
+  grid-template-columns: 1fr;
 
 
-  .basket{
+  /* .basket{
     background: pink;
-  }
+  } */
 
   
 
